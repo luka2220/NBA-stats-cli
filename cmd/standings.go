b23f67c131ebf04381ed6144a43a4672a174fa66
Luka Piplica <42144047+luka2220@users.noreply.github.com>
2024-01-26 06:32:05 -0500

    Standings command and service completed.

--- cmd/standings.go
+++ cmd/standings.go
@@ -12,6 +12,7 @@ import (
 	"net/http"
 	"os"
 
+	"github.com/joho/godotenv"
 	"github.com/olekukonko/tablewriter"
 	"github.com/spf13/cobra"
 )
@@ -21,7 +22,6 @@ const (
 	apiUrl        = "https://api-basketball.p.rapidapi.com/standings?league=12&season=2023-2024"
 	apiKeyHeader  = "X-RapidAPI-Key"
 	apiHostHeader = "X-RapidAPI-Host"
-	apiKey        = "API_KEY"
 )
 
 // Handles interactions with the standings API
@@ -45,7 +45,7 @@ func (s *StandingsService) FetchStandings() (*models.Standings, error) {
 		return nil, err
 	}
 
-	req.Header.Add(apiKeyHeader, apiKey)
+	req.Header.Add(apiKeyHeader, os.Getenv("API_KEY"))
 	req.Header.Add(apiHostHeader, "api-basketball.p.rapidapi.com")
 
 	res, err := http.DefaultClient.Do(req)
@@ -55,6 +55,8 @@ func (s *StandingsService) FetchStandings() (*models.Standings, error) {
 	defer res.Body.Close()
 
 	var standings models.Standings
+
+	// Convert JSON data in the defined models
 	err = json.NewDecoder(res.Body).Decode(&standings)
 	if err != nil {
 		return nil, err
@@ -65,9 +67,15 @@ func (s *StandingsService) FetchStandings() (*models.Standings, error) {
 
 // DisplayStandingsTable displays the standings in a table
 func DisplayStandingsTable(standings *models.Standings) {
-	// Display standings in table
-	table := tablewriter.NewWriter(os.Stdout)
-	table.SetHeader([]string{"Position", "Conference", "Team", "W", "L", "PCT"})
+	// Create West conference standings
+	westConference := tablewriter.NewWriter(os.Stdout)
+	westConference.SetHeader([]string{"Position", "Conference", "Team", "W", "L", "PCT"})
+	westConference.SetCaption(true, "West Standings")
+
+	// Create East conference standings
+	eastConference := tablewriter.NewWriter(os.Stdout)
+	eastConference.SetHeader([]string{"Position", "Conference", "Team", "W", "L", "PCT"})
+	eastConference.SetCaption(true, "East Standings")
 
 	for _, response := range standings.Response {
 		for _, r := range response {
@@ -79,13 +87,17 @@ func DisplayStandingsTable(standings *models.Standings) {
 			losses := fmt.Sprintf("%d", r.Games.Lose.Total)
 			winPercentage := r.Games.Win.Percentage
 
-			// Append a slice of strings to the table
-			table.Append([]string{position, conference, team, wins, losses, winPercentage})
+			if conference == "Western Conference" {
+				westConference.Append([]string{position, conference, team, wins, losses, winPercentage})
+			} else if conference == "Eastern Conference" {
+				eastConference.Append([]string{position, conference, team, wins, losses, winPercentage})
+			}
 		}
 	}
 
-	// Display the table
-	table.Render()
+	// Display tables
+	westConference.Render()
+	eastConference.Render()
 }
 
 // standingsCmd represents the standings command
@@ -95,6 +107,13 @@ var standingsCmd = &cobra.Command{
 	Long:  "Nba stat data displayed in table format",
 
 	Run: func(cmd *cobra.Command, args []string) {
+		// Load in env variables
+		err := godotenv.Load()
+		if err != nil {
+			fmt.Println("An Error Occurred: ", err)
+			return
+		}
+
 		standingsService := NewStandingsService()
 
 		standings, err := standingsService.FetchStandings()
